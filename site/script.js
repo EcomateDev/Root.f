be08a1242e198c87039dfedcdae898f656bb0b5c
EcomateDev <144458220+EcomateDev@users.noreply.github.com>
2023-12-10 19:40:15 +0100

    Delete site directory

--- site/script.js
+++
@@ -1,8 +0,0 @@
-// Код для зміни теми на темну та зворотно
-
-const themeToggle = document.getElementById('theme-toggle');
-const body = document.body;
-
-themeToggle.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-});
